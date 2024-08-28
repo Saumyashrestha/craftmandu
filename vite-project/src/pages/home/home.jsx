3800b6149f1ef71fe25a743fcab8eac121587ed7
Abh03 <abhyu.adhikari@gmail.com>
2024-08-28 19:54:36 +0545

    cards

--- vite-project/src/pages/home/home.jsx
+++ vite-project/src/pages/home/home.jsx
@@ -1,10 +1,14 @@
+import Category from "../../components/category/category";
 import HeroSection from "../../components/heroSection/heroSection";
+import HomeProductCard from "../../components/homeProductCard/homeProductCard";
 import Layout from "../../components/layout/layout";
 
 const Home = () =>{
     return(
         <Layout>
             <HeroSection/>
+            <Category/>
+            <HomeProductCard/>
         </Layout>
     );
 }
