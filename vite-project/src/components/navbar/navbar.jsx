7ea06da870cdb93061f91d42b068f0a22e9cddf8
Saumya <Saumya@gmail.com>
2024-11-06 00:04:37 +0545

    delete not working

--- vite-project/src/components/navbar/navbar.jsx
+++ vite-project/src/components/navbar/navbar.jsx
@@ -16,15 +16,42 @@ const Navbar = () => {
     const logout = () => {
         localStorage.clear('users');
         navigate("/login")
+
+    
     }
 
     //cartItems
     const cartItems = useSelector((state) => state.cart);
     
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [settingsDropdownOpen, setSettingsDropdownOpen] = useState(false);
 
-    // toggle dropdown
-    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newName, setNewName] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    
+
+     // toggle functions
+     const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+     const toggleSettingsDropdown = () => setSettingsDropdownOpen(!settingsDropdownOpen);
+     const toggleModal = () => setIsModalOpen(!isModalOpen);
+     const toggleDeleteModal = () => setIsDeleteModalOpen(!isDeleteModalOpen);
+ 
+     const handleSaveChanges = () => {
+         // Handle save changes logic here
+         alert(`Saving changes for Name: ${newName}, Email: ${newEmail}`);
+         toggleModal(); // Close the modal after saving
+     };
+
+
+     const handleDeleteAccount = () => {
+        // Insert your delete account logic here, such as API call or localStorage cleanup
+        localStorage.removeItem('users');
+        setIsDeleteModalOpen(false); // Close delete confirmation modal
+        navigate("/signup"); // Redirect after account deletion
+    };
 
     // navList Data
     const navListLeft = (
@@ -62,12 +89,31 @@ const Navbar = () => {
                      onClick={toggleDropdown}
                  ></i>
                  {dropdownOpen && (
-                     <ul className="absolute top-12 left-1/2 transform -translate-x-1/2 bg-white shadow-lg rounded-md w-40 text-md">
+                     <ul className="absolute top-12 left-0 transform -translate-x-1/2 bg-white shadow-lg rounded-md w-32 text-md">
                          <li>
-                             <Link to={'/profile'} className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Profile</Link>
+                         <Link 
+                                    to={user.role === "admin" ? "/admin-dashboard" : "/user-dashboard"} 
+                                    className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                                >Profile</Link>
                          </li>
-                         <li>
-                             <Link to={'/settings'} className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Settings</Link>
+                         <li className="relative">
+                         <span 
+                                    className="block px-4 py-2 text-gray-800 hover:bg-gray-200 flex justify-between items-center cursor-pointer"
+                                    onClick={toggleSettingsDropdown}
+                                >Settings
+                                <i className="fas fa-chevron-right ml-2"></i>
+                                </span>
+                                {settingsDropdownOpen && (
+                                    <ul className="absolute top-0 left-full ml-2 bg-white shadow-lg rounded-md w-32 text-md">
+                                        <li>
+                                        <span onClick={toggleModal} className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">Edit Profile</span>
+                                        </li>
+                                        <hr className="border-t border-gray-300 my-1" />
+                                        <li>
+                                            <Link to={'/delete-account'} className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Delete Account</Link>
+                                        </li>
+                                    </ul>
+                                )}
                          </li>
                          <hr className="border-t border-gray-300 my-1" />
                          <li className="cursor-pointer" onClick={logout}>
@@ -77,6 +123,7 @@ const Navbar = () => {
                  )}
              </li>
             )}
+
 
 
             {user?.role === "admin" && <li>
@@ -123,7 +170,46 @@ const Navbar = () => {
                             </div>
                         </div>
                     </div>
-              
+               {isModalOpen && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="relative bg-[#f9f9f9] border-2 border-[#dd3333] shadow-md rounded-lg w-1/3 p-6">
+                        <button onClick={toggleModal} className="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
+                            <i className="fas fa-times"></i>
+                        </button>
+                        <h2 className="text-2xl font-semibold text-center text-[#dd3333] mb-6">Edit Your Profile</h2>
+                        <div className="mb-4">
+                            <label className="block mb-1 text-sm text-[#dd3333] font-semibold">Name</label>
+                            <input type="text" placeholder="Enter Your New Name" value={newName} onChange={(e) => setNewName(e.target.value)} className="w-full p-2 border border-[#dd3333] shadow-md rounded-md" />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-1 text-sm text-[#dd3333] font-semibold">Email</label>
+                            <input type="email" placeholder="Enter Your New E-mail" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} className="w-full p-2 border border-[#dd3333] shadow-md rounded-md" />
+                        </div>
+                        <div className="flex flex-col">
+                            <button className="w-full py-3 bg-[#dd3333] text-white rounded-md font-semibold hover:bg-[#ff4444]" onClick={handleSaveChanges}>Save Changes</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {/* Delete Account Confirmation Modal */}
+            {isDeleteModalOpen && (
+                    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                        <div className="relative bg-[#f9f9f9] border-2 border-[#dd3333] shadow-md rounded-lg w-1/3 p-6">
+                            {/* Close (Cross) Icon */}
+                            <button onClick={toggleDeleteModal} className="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
+                                <i className="fas fa-times"></i>
+                            </button>
+                            <h2 className="text-2xl font-semibold text-center text-[#dd3333] mb-6">Delete Your Account?</h2>
+                            <p className="text-center text-gray-600 mb-6">Are you sure you want to delete your account? This action cannot be undone.</p>
+                            <div className="flex justify-around">
+                                {/* Delete Button */}
+                                <button onClick={handleDeleteAccount} className="px-4 py-2 bg-[#dd3333] text-white rounded-md font-semibold hover:bg-[#ff4444]">Delete</button>
+                                {/* Cancel Button */}
+                                <button onClick={toggleDeleteModal} className="px-4 py-2 bg-gray-200 text-gray-700 rounded-md font-semibold hover:bg-gray-300">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </nav>
     );
