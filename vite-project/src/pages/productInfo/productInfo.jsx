5a63a743f8765ca2807735e18c97306e29d06af1
Abh03 <abhyu.adhikari@gmail.com>
2024-11-13 20:23:51 +0545

    sth sth and sth sth anf filter

--- vite-project/src/pages/productInfo/productInfo.jsx
+++ vite-project/src/pages/productInfo/productInfo.jsx
@@ -83,90 +83,24 @@ const ProductInfo = () => {
                                             <h2 className="max-w-xl mb-6 text-xl font-semibold leading-loose tracking-wide text-[#dd3333] md:text-2xl dark:text-gray-300">
                                                 {product?.title}
                                             </h2>
-                                            <div className="flex flex-wrap items-center mb-6">
-                                                <ul className="flex mb-4 mr-2 lg:mb-0">
-                                                    <li>
-                                                        <a href="">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                width={16}
-                                                                height={16}
-                                                                fill="currentColor"
-                                                                className="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                                viewBox="0 0 16 16"
-                                                            >
-                                                                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                width={16}
-                                                                height={16}
-                                                                fill="currentColor"
-                                                                className="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                                viewBox="0 0 16 16"
-                                                            >
-                                                                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                width={16}
-                                                                height={16}
-                                                                fill="currentColor"
-                                                                className="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                                viewBox="0 0 16 16"
-                                                            >
-                                                                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                width={16}
-                                                                height={16}
-                                                                fill="currentColor"
-                                                                className="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                                viewBox="0 0 16 16"
-                                                            >
-                                                                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
                                             <p className="inline-block text-2xl font-semibold text-gray-700 dark:text-gray-400 ">
                                                 <span>Rs. {product?.price}</span>
                                             </p>
                                         </div>
+                                        <div className="border border-[#dd3333] shadow-md bg-[#f9f9f9] p-4 rounded-lg mb-6">
                                         <div className="mb-6">
                                             <h2 className="mb-2 text-lg font-bold text-[#dd3333] dark:text-gray-400">
                                                 Description:
                                             </h2>
                                             <p>{product?.description}</p>
                                         </div>
-                                        <div className="border border-[#dd3333] shadow-md bg-[#f9f9f9] p-4 rounded-lg mb-6">
                                     <p className="font-semibold text-gray-700 dark:text-gray-400 mb-2">
-                                        Product Code: 1247
+                                        Product Code: {product?.productCode}
                                     </p>
                                     <div className="flex items-center">
                                         <p className="font-semibold text-gray-700 dark:text-gray-400 mr-4">
-                                            Size:
+                                            Size: {product?.size}
                                         </p>
-                                       <input
-                                       type="text"
-                                       placeholder="Dimensions (height x breadth x width (in mm))"
-                                       className="w-full px-4 py-2 border border-gray-300 rounded-lg"
-                                      
-                                       />
                                     </div>
                                 </div>
                                         <div className="mb-6 " />
